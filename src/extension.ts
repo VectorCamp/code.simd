8d649813282682a6c71ba9a3d8565f72f47d8591
gtsoul-tech <gtsoulkanakis@gmail.com>
2025-11-18 10:42:53 +0200

    remove ai,pump version,rename

--- src/extension.ts
+++ src/extension.ts
@@ -31,21 +31,7 @@ import { registerShowPerformanceGraphCommand } from './showPerformanceGraph';
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Extension "code.simd.ai" is now active!');
-	const provider = new ChatViewProvider(context);
-	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider('code.simd.ai.chatView', provider)
-	);
-
-	registerEditSIMDCommand(context, provider);
-	registerAcceptRejectCommands(context);
-	context.subscriptions.push(
-		registerTranslateCommand('SSE4.2', 'NEON', 'code.simd.ai.translateSSEToNeon'),
-		registerTranslateCommand('NEON', 'SSE4.2', 'code.simd.ai.translateNeonToSSE'),
-		registerTranslateCommand('SSE4.2', 'VSX', 'code.simd.ai.translateSSEToVSX'),
-		registerTranslateCommand('VSX', 'SSE4.2', 'code.simd.ai.translateVSXToSSE'),
-		registerTranslateCommand('NEON', 'VSX', 'code.simd.ai.translateNeonToVSX'),
-		registerTranslateCommand('VSX', 'NEON', 'code.simd.ai.translateVSXToNeon')
-	);
+	
 	context.subscriptions.push(
 		vscode.languages.registerCodeLensProvider({ scheme: 'file', language: '*' }, new TranslationCodeLensProvider())
 	);
